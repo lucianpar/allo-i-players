a19df9855de574061eef61750068a06c2b4c2ca3
Lucian <lucianparisi@gmail.com>
2026-02-10 14:18:50 -0800

    fixed jack issue in cmake in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir -p build/Release
-cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 -B build/Release -S .
-
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DRTAUDIO_API_JACK=OFF -DRTMIDI_API_JACK=OFF -Wno-deprecated -DBUILD_EXAMPLES=0 -B build/Release -S .
 (
   # utilizing cmake's parallel build options
   # for cmake >= 3.12: -j <number of processor cores + 1>
